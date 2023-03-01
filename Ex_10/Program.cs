688e6c479d0a79dc51e1df4a9a214b10765128a0
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-01 15:55:13 +0300

    Задача 10

---
+++ Ex_10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое трехзначное число: ");
+int userNumber;
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out userNumber))
+        break;
+    Console.WriteLine("Ошибка ввода");
+}
+int result = (userNumber / 10) % 10;
+Console.WriteLine($"Вторая цифра числа {userNumber} {result}");
