26b6901f93b0a7eaa8e0bcb02559b326261820bf
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-01 16:36:29 +0300

    Задача 13

---
+++ Ex_13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Введите целое число: ");
+int userNumber;
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out userNumber))
+        break;
+    Console.WriteLine("Ошибка ввода");
+}
+string userNumberText = Convert.ToString(userNumber);
+if (userNumberText.Length>2)
+{
+    Console.WriteLine($"Третья цифра числа {userNumber} - {userNumberText[2]}");
+}
+else
+{
+Console.WriteLine($"Третьей цифры в числе {userNumber} нет");
+}
