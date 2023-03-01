ad9becd2c8e88e1d7123a9d17f8c476668abf415
«Svetlana <shemetillo.svetlana@yandex.ru>
2023-03-01 16:52:42 +0300

    Задача 15

---
+++ Ex_15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите цифру, означающую день недели: ");
+int userNumber;
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out userNumber))
+        break;
+    Console.WriteLine("Ошибка ввода");
+}
+if (userNumber == 6 || userNumber == 7)
+{
+    Console.WriteLine("Этот день является выходным");
+}
+else if (userNumber>0 && userNumber<6)
+{
+    Console.WriteLine("Этот день не явялется выходным");
+}
+else
+    Console.WriteLine("Эта цифра не означает день недели");
